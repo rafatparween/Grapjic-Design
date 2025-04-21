fefee69261fb6981d693a8dca3a6e9eefe5ee644
rafatparween <rafatparween27392@gmail.com>
2025-04-22 01:59:07 +0530

    change

---
+++ app/serviceR/page.js
@@ -0,0 +1,20 @@
+import Footer from "../Components/Footer";
+import Diagnose from "./pages/Digonose";
+import PageHeader from "./pages/PageHeader";
+import PageNavbar from "./pages/PageNavbar";
+import Troubleshoot from "./pages/Troubleshoot";
+
+export default function Home() {
+    return (
+      <>
+      <PageNavbar/>
+      <PageHeader/>
+      <Troubleshoot/>
+      <Diagnose/>
+      <Footer/>
+
+       
+      </>
+    );
+  }
+  
